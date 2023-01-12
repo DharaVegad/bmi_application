11fe4e004cf7bbc475c7e22e8b9e69b2dfb55159
DharaVegad <75181096+DharaVegad@users.noreply.github.com>
2023-01-11 21:12:07 -0500

    new

---
+++ BMI_CALCULATOR_NEW/app/src/main/java/com/example/bmi_calculator_new/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.bmi_calculator_new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText height;
+    private EditText weight;
+    private TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        result = (TextView) findViewById(R.id.result);
+    }
+
+    public void calculateBMI(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)
+                && weightStr != null  &&  !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            displayBMI(bmi);
+        }
+    }
+
+    private void displayBMI(float bmi) {
+        String bmiLabel = "";
+
+        bmiLabel = bmi + "\n\n" + bmiLabel;
+        result.setText(bmiLabel);
+    }
+}
